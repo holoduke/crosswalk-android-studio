da3eee8e5b2301c86259c95dc36d1ef2a90035d7
Gillis Haasnoot <gillis@l256.local>
2015-06-25 16:23:02 +0200

    first commit

---
+++ app/src/main/java/manager/football/footballmaniamanager/MainActivity.java
@@ -0,0 +1,49 @@
+package manager.football.footballmaniamanager;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import org.xwalk.core.XWalkActivity;
+import org.xwalk.core.XWalkView;
+
+
+public class MainActivity extends XWalkActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onXWalkReady() {
+        XWalkView xWalkView=(XWalkView)findViewById(R.id.xwalkWebView);
+        xWalkView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        xWalkView.load("http://stereobit.github.io/dragend/", null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
